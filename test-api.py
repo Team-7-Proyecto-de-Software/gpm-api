397a95de638d5877eb0a3323d290e0d3d037c244
gonzalo <gonzalo.javier.cartes@gmail.com>
2025-03-26 17:51:39 -0300

    Primera descarga
    Se modifica el archivo test-api para descargar los primeros archivos con la API.

--- test-api.py
+++ test-api.py
@@ -1,5 +1,27 @@
 import gpm
+import gpm
+import datetime
 
+# Specify the time period you are interested in
+start_time = datetime.datetime.strptime("2020-08-01 12:00:00", "%Y-%m-%d %H:%M:%S")
+end_time = datetime.datetime.strptime("2020-08-02 12:00:00", "%Y-%m-%d %H:%M:%S")
+# Specify the product and product type
+product = "2A-MHS-METOPB-CLIM"  # "2A-GMI-CLIM", "2A-SSMIS-F17-CLIM", ...
+product_type = "RS"
+storage = "GES_DISC"
+# Specify the version
+version = 7
 
-configs = gpm.read_configs()
-print(configs)
+# Download the data
+gpm.download(
+    product=product,
+    product_type=product_type,
+    version=version,
+    start_time=start_time,
+    end_time=end_time,
+    storage=storage,
+    force_download=False,
+    verbose=True,
+    progress_bar=True,
+    check_integrity=False,
+)
